bfcf04bc8bb3e42e24f49272ac4252fc82111a8f
mkrivozyateva <mkrivozyateva@gmail.com>
2016-12-16 15:27:58 +0700

    Update error.php

--- backend/views/site/error.php
+++ backend/views/site/error.php
@@ -1,27 +1,14 @@
 <?php
-
 /* @var $this yii\web\View */
 /* @var $name string */
 /* @var $message string */
 /* @var $exception Exception */
-
 use yii\helpers\Html;
-
-$this->title = $name;
+$this->title = 'Ошибка';
 ?>
 <div class="site-error">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
+    <h1>Ошибка <?= $exception->statusCode ?></h1>
     <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+        <?= (Html::encode($message))?>
     </div>
-
-    <p>
-        Произошла ошибка веб-сервера во время обработки вашего запроса.
-    </p>
-    <p>
-        Пожалуйста, свяжитесь с нами, если вы думаете, что это ошибка сервера. Спасибо!
-    </p>
-
 </div>
